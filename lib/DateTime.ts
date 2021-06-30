cf45bec101259ef8a10bf6300d3e679d1f8e4ed5
macleysousa <macley_321@hotmail.com>
2021-06-30 09:06:33 -0300

    init

---
+++ lib/DateTime.ts
@@ -0,0 +1,197 @@
+
+export class DateTime {
+
+    constructor(DateTime?: string | number | Date) {
+
+        if (typeof DateTime == 'string') {
+            var date = new Date(DateTime);
+            if (date.getTimezoneOffset() > 0) {
+                this.DateTimeUtc = new Date(date.setMinutes(date.getMinutes() + (date.getTimezoneOffset())))
+                this.setDate(new Date(date.setMinutes(date.getMinutes() - (date.getTimezoneOffset()))));
+            } else {
+                this.DateTimeUtc = date;
+                this.setDate(date);
+            }
+        }
+        else if (typeof DateTime == 'number') {
+            var date = new Date(new Date(0).setSeconds(1625051235))
+            this.DateTimeUtc = new Date(date.toUTCString());
+            this.setDate(new Date(date.setMinutes(date.getMinutes() - (date.getTimezoneOffset()))));
+        }
+        else if (typeof DateTime == 'object') {
+            this.DateTimeUtc = new Date(DateTime.toUTCString());
+            var date = new Date(DateTime.setMinutes(DateTime.getMinutes() - (DateTime.getTimezoneOffset())))
+            this.setDate(date);
+        }
+    }
+    private DateTime: Date = new Date('0001-01-01T00:00:00.000Z');
+    private DateTimeUtc: Date = new Date('0001-01-01T03:00:00.000Z');
+
+    public Year: number = 0;
+    public Months: number = 0;
+    public Day: number = 0;
+
+    public Hours: number = 0;
+    public Minutes: number = 0;
+    public Seconds: number = 0;
+    public Milliseconds: number = 0;
+
+    public TimezoneOffSet: number = 0;
+
+    public static Now = new Date(new Date().getTime() - (new Date().getTimezoneOffset() * 60000));
+    public static UtcNow = new Date();
+
+    public DayOfWeek: number = 0;
+
+    private setDate(dateTime: Date) {
+        this.DateTime = dateTime;
+
+        this.Year = dateTime.getUTCFullYear();
+        this.Months = dateTime.getUTCMonth() + 1;
+        this.Day = dateTime.getUTCDate();
+
+        this.Hours = dateTime.getUTCHours();
+        this.Minutes = dateTime.getUTCMinutes();
+        this.Seconds = dateTime.getUTCSeconds();
+        this.Milliseconds = dateTime.getUTCMilliseconds();
+
+        this.TimezoneOffSet = dateTime.getTimezoneOffset();
+
+        this.DayOfWeek = dateTime.getUTCDay();
+    }
+
+    AddYears(value = 0) {
+        var result = this.DateTime;
+        this.setDate(new Date(result.setFullYear(this.DateTime.getFullYear() + value)));
+        return this.DateTime;
+    }
+
+    AddMonths(value = 0) {
+        var result = this.DateTime;
+        this.setDate(new Date(result.setMonth(this.DateTime.getMonth() + value)));
+        return this.DateTime;
+    }
+
+    AddDays(value = 0) {
+        var result = this.DateTime;
+        this.setDate(new Date(result.setDate(this.DateTime.getDate() + value)));
+        return this.DateTime;
+    }
+
+    AddHours(value = 0) {
+        var result = this.DateTime;
+        this.setDate(new Date(result.setHours(this.DateTime.getHours() + value)));
+        return this.DateTime;
+    }
+
+    AddMinutes(value = 0) {
+        var result = this.DateTime;
+        this.setDate(new Date(result.setMinutes(this.DateTime.getMinutes() + value)));
+        return this.DateTime;
+    }
+
+    AddSeconds(value = 0) {
+        var result = this.DateTime;
+        this.setDate(new Date(result.setSeconds(this.DateTime.getSeconds() + value)));
+        return this.DateTime;
+    }
+
+    AddMilliseconds(value = 0) {
+        var result = this.DateTime;
+        this.setDate(new Date(result.setMilliseconds(this.DateTime.getMilliseconds() + value)));
+        return this.DateTime;
+    }
+
+    getDateTime() {
+        return this.DateTime;
+    }
+
+    getDateTimeUtc() {
+        return this.DateTimeUtc;
+    }
+
+    getTimestamp() {
+        return this.DateTime.getTime() / 1000;
+    }
+
+    ToString(format: format = undefined, culture: culture = undefined) {
+
+        var date = this.DateTimeUtc.toLocaleDateString(culture);
+        var time = this.DateTimeUtc.toLocaleTimeString(culture);
+
+        const hours = this.Hours >= 9 ? `${this.Hours}` : `0${this.Hours}`;
+        const minutes = this.Minutes >= 9 ? `${this.Minutes}` : `0${this.Minutes}`;
+        const seconds = this.Seconds >= 9 ? `${this.Seconds}` : `0${this.Seconds}`;
+
+        switch (format) {
+            case 'd':
+                return date;
+            case 't':
+                return `${hours}:${minutes}`;
+            case 'T':
+                return time;
+            case 'g':
+                return `${date} ${time}:${minutes}`;
+            case 'G':
+                return `${date} ${hours}:${minutes}:${seconds}`;
+            case 'json':
+                return this.DateTime.toJSON();
+            case 'UTC':
+                return this.DateTimeUtc.toJSON();
+            default:
+                return `${date} ${time}`;
+        }
+    }
+
+    ToStringCustom(format: string = "") {
+        var yy = `${this.Year.toString().substring(2, 4)}`;
+        var yyyy = `${this.Year}`;
+        var M = `${this.Months}`;
+        var MM = this.Months >= 9 ? `${this.Months}` : `0${this.Months}`;
+        var d = `${this.Day}`;
+        var dd = this.Day >= 9 ? `${this.Day}` : `0${this.Day}`;
+        var h = "";
+        var hh = "";
+        var m = "";
+        var mm = "";
+        var s = "";
+        var ss = "";
+        var ms = "";
+
+        let date = format;
+        date = date.replace('yyyy', yyyy);
+        date = date.replace('yy', yy).replace('y', '');
+
+        date = date.replace('MM', MM);
+        date = date.replace('M', M).replace('M', '');
+
+        date = date.replace('dd', dd);
+        date = date.replace('d', d).replace('d', '');
+
+        date = date.replace('hh', hh);
+        date = date.replace('h', h).replace('h', '');
+
+        date = date.replace('mm', mm);
+        date = date.replace('m', m).replace('m', '');
+
+        date = date.replace('ss', ss);
+        date = date.replace('s', s).replace('s', '');
+
+        date = date.replace('ms', ms);
+
+        return date;
+
+    }
+
+    ToLongDateString() {
+        return this.DateTimeUtc.toString();
+    }
+
+    ToLongTimeString() {
+        return this.DateTimeUtc.toTimeString();
+    }
+
+}
+
+type format = 'json' | 'd' | 'D' | 't' | 'T' | 'g' | 'G' | 'UTC';
+type culture = 'pt-BR' | 'en-US';
