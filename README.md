ce831f5890b817a42c1e683c4d86656c7cee8d43
macleysousa <macley_321@hotmail.com>
2021-07-01 07:24:07 -0300

    v1.0.5

--- README.md
+++ README.md
@@ -23,34 +23,34 @@ var DateTime = require('./index').DateTime
 
 ```json
 {
-  DateTime: {
-    datetime: {
-      DateTime: 2021-06-30T17:03:35.036Z,
-      DateTimeUtc: 2021-06-30T20:03:35.000Z,
-      Timestamp: 1625072616
+  "DateTime": {
+    "datetime": {
+      "DateTime":"2021-06-30T17:03:35.036",
+      "DateTimeUtc":"2021-06-30T20:03:35.000",
+      "Timestamp": 1625072616
     },
-    string: {
-      DateTime: '30/06/2021 17:03:35',
-      Json: '2021-06-30T17:03:35.036Z',
-      D: '30/06/2021 17:03:35',
-      d: '30/06/2021',
-      t: '17:03',
-      T: '17:03:35',
-      g: '30/06/2021 17:03:35:03',
-      G: '30/06/2021 17:03:35',
-      UTC: '2021-06-30T20:03:35.000Z',
-      Custom: '30-06',
-      LongDate: 'Wed Jun 30 2021 17:03:35 GMT-0300 (Horário Padrão de Brasília)',
-      LongTime: '17:03:35 GMT-0300 (Horário Padrão de Brasília)'
+    "string": {
+      "DateTime": "30/06/2021 17:03:35",
+      "Json": "2021-06-30T17:03:35.036Z",
+      "D": "30/06/2021 17:03:35",
+      "d": "30/06/2021",
+      "t": "17:03",
+      "T": "17:03:35",
+      "g": "30/06/2021 17:03:35:03",
+      "G": "30/06/2021 17:03:35",
+      "UTC": "2021-06-30T20:03:35.000Z",
+      "Custom": "30-06",
+      "LongDate": "Wed Jun 30 2021 17:03:35 GMT-0300 (Horário Padrão de Brasília)",
+      "LongTime": "17:03:35 GMT-0300 (Horário Padrão de Brasília)"
     },
-    add: {
-      AddYears: 2001-01-01T00:00:00.000Z,
-      AddMonths: 2000-02-01T00:00:00.000Z,
-      AddDays: 2000-01-02T00:00:00.000Z,
-      AddHours: 2000-01-01T01:00:00.000Z,
-      AddMinutes: 2000-01-01T00:01:00.000Z,
-      AddSeconds: 2000-01-01T00:00:01.000Z,
-      AddMilliseconds: 2000-01-01T00:00:00.001Z
+    "add": {
+      "AddYears": "2001-01-01T00:00:00.000Z",
+      "AddMonths": "2000-02-01T00:00:00.000Z",
+      "AddDays": "2000-01-02T00:00:00.000Z",
+      "AddHours": "2000-01-01T01:00:00.000Z",
+      "AddMinutes": "2000-01-01T00:01:00.000Z",
+      "AddSeconds": "2000-01-01T00:00:01.000Z",
+      "AddMilliseconds": "2000-01-01T00:00:00.001Z"
     }
   }
 }
--- test.js
+++ test.js
@@ -1,40 +1,41 @@
+
+//#region DateTime
 var DateTime = require('./index').DateTime;
+const datetime = new DateTime('2000-01-01T00:00:00.000Z');
 
-const datetime = new DateTime(new Date());
-console.log({
-    DateTime: {
-        datetime: {
-            DateTime: datetime.DateTime,
-            DateTimeUtc: datetime.DateTimeUtc,
-            Timestamp: datetime.Timestamp
-        },
-        string: {
-            DateTime: datetime.ToString(),
-            Json: datetime.ToString('json'),
-            D: datetime.ToString('D'),
-            d: datetime.ToString('d'),
-            t: datetime.ToString('t'),
-            T: datetime.ToString('T'),
-            g: datetime.ToString('g'),
-            G: datetime.ToString('G'),
-            UTC: datetime.ToString('UTC'),
-            Custom: datetime.ToStringCustom('dd-MM'),
-            LongDate: datetime.ToLongDateString(),
-            LongTime: datetime.ToLongTimeString()
-        },
-        add: {
-            AddYears: new DateTime('2000-01-01T00:00:00.000Z').AddYears(1).DateTime,
-            AddMonths: new DateTime('2000-01-01T00:00:00.000Z').AddMonths(1).DateTime,
-            AddDays: new DateTime('2000-01-01T00:00:00.000Z').AddDays(1).DateTime,
-            AddHours: new DateTime('2000-01-01T00:00:00.000Z').AddHours(1).DateTime,
-            AddMinutes: new DateTime('2000-01-01T00:00:00.000Z').AddMinutes(1).DateTime,
-            AddSeconds: new DateTime('2000-01-01T00:00:00.000Z').AddSeconds(1).DateTime,
-            AddMilliseconds: new DateTime('2000-01-01T00:00:00.000Z').AddMilliseconds(1).DateTime,
-        }
-    }
-});
+// console.log({
+//     DateTime: {
+//         datetime: {
+//             DateTime: datetime.DateTime,
+//             DateTimeUtc: datetime.DateTimeUtc,
+//             Timestamp: datetime.Timestamp
+//         },
+//         string: {
+//             DateTime: datetime.ToString(),
+//             Json: datetime.ToString('json'),
+//             D: datetime.ToString('D'),
+//             d: datetime.ToString('d'),
+//             t: datetime.ToString('t'),
+//             T: datetime.ToString('T'),
+//             g: datetime.ToString('g'),
+//             G: datetime.ToString('G'),
+//             UTC: datetime.ToString('UTC'),
+//             Custom: datetime.ToStringCustom('dd-MM'),
+//             LongDate: datetime.ToLongDateString(),
+//             LongTime: datetime.ToLongTimeString()
+//         },
+//         add: {
+//             AddYears: new DateTime('2000-01-01T00:00:00.000Z').AddYears(1).DateTime,
+//             AddMonths: new DateTime('2000-01-01T00:00:00.000Z').AddMonths(1).DateTime,
+//             AddDays: new DateTime('2000-01-01T00:00:00.000Z').AddDays(1).DateTime,
+//             AddHours: new DateTime('2000-01-01T00:00:00.000Z').AddHours(1).DateTime,
+//             AddMinutes: new DateTime('2000-01-01T00:00:00.000Z').AddMinutes(1).DateTime,
+//             AddSeconds: new DateTime('2000-01-01T00:00:00.000Z').AddSeconds(1).DateTime,
+//             AddMilliseconds: new DateTime('2000-01-01T00:00:00.000Z').AddMilliseconds(1).DateTime,
+//         }
+//     }
+// });
 
-// var Convert = require('./index').Convert;
+//#endregion
 
-// const convert = new Convert();
-// console.log(convert);
+
