67ae012e1a3913ff5fc7f684bb6928e9eb762aa1
Macley Sousa <macleysousa@hotmail.com>
2023-05-11 09:28:39 -0300

    types v2.1.0

--- dist/lib/convert/index.d.ts
+++ dist/lib/convert/index.d.ts
@@ -12,11 +12,11 @@ declare class ConvertConstructor {
         timezone?: string;
         default?: Date;
     } | undefined): Date;
-    toDecimal(value: any): Number | number;
+    toDecimal(value: any): number;
     toDecimal(value: any, options?: {
         culture?: Culture | undefined;
         default?: Number | undefined;
-    } | undefined): Number | number;
+    } | undefined): number;
     toNumber(value: any): number;
     toNumber(value: any, options?: {
         culture?: Culture;
