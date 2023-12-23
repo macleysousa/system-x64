255c331325b38e2940e6df1ba97f532cf09fdf24
Macley Sousa <macleysousa@hotmail.com>
2023-12-23 14:26:03 -0300

    Add date comparison methods to Date prototype

--- extends/date.ts
+++ extends/date.ts
@@ -4,6 +4,13 @@ interface Date {
     addMinutes(minutes: number): Date;
     addSeconds(seconds: number): Date;
     format(fmt: string): string;
+    diffInMilliseconds(date: Date): number;
+    diffInSeconds(date: Date): number;
+    diffInMinutes(date: Date): number;
+    diffInHours(date: Date): number;
+    diffInDays(date: Date): number;
+    diffInMonths(date: Date): number;
+    diffInYears(date: Date): number;
 }
 
 Date.prototype.addDays = function (days: number): Date {
@@ -93,4 +100,32 @@ Date.prototype.format = function (fmt: string): string {
     formattedString = formattedString.replace(/ss/g, replaceValue('ss', 'ss'));
 
     return formattedString;
+};
+
+Date.prototype.diffInMilliseconds = function (date: Date): number {
+    return Math.abs(this.valueOf() - date.valueOf());
+};
+
+Date.prototype.diffInSeconds = function (date: Date): number {
+    return this.diffInMilliseconds(date) / 1000;
+};
+
+Date.prototype.diffInMinutes = function (date: Date): number {
+    return this.diffInSeconds(date) / 60
+};
+
+Date.prototype.diffInHours = function (date: Date): number {
+    return this.diffInMinutes(date) / 60;
+};
+
+Date.prototype.diffInDays = function (date: Date): number {
+    return this.diffInHours(date) / 24;
+};
+
+Date.prototype.diffInMonths = function (date: Date): number {
+    return this.diffInDays(date) / 30;
+};
+
+Date.prototype.diffInYears = function (date: Date): number {
+    return this.diffInDays(date) / 365;
 };
